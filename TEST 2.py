c25586b8934241e14868bbbf9e9f7cb8c011b4eb
Kingsley703 <160226678+Kingsley703@users.noreply.github.com>
2024-02-16 10:53:04 +0000

    TEST 2.py

--- TEST 2.py
+++ TEST 2.py
@@ -9,12 +9,15 @@ for user in new_users:
 
 
 #FizzBuzz
-for num in range(1,101):
-	if num % 3 == 0 and num % 5 == 0:
-		print("Fizz")
-
-	elif num % 5 == 0:
-		print("Buzz")
-
-	else:
-		print(num)
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+	    
+    elif num % 3 == 0:
+        print("Fizz")
+	    
+    elif num % 5 == 0:
+        print("Buzz")
+	    
+    else:
+        print(num)
